ce63fabd3ae058712eec938d766486c79d9dded0
EGRrqq <egrrqqdev@gmail.com>
2025-09-13 14:34:51 +0500

    refactor: split toolbox into smaller components
    refactor: remove useless markup

--- components/Toolbox.tsx
+++ components/Toolbox.tsx
@@ -6,7 +6,7 @@ interface IToolEntity {
   // if limit not specified it === infinity
   limit?: number;
   disabled: boolean;
-  key: string[];
+  keyBindings: string[];
 
   src: string;
   alt: string;
@@ -19,7 +19,7 @@ const TOOLS: Record<IToolKey, IToolEntity> = {
   roof: {
     limit: 1,
     disabled: true,
-    key: ["3"],
+    keyBindings: ["3"],
     src: "./icons/roof_icon.svg",
     alt: "roof icon",
     width: 40.21,
@@ -28,7 +28,7 @@ const TOOLS: Record<IToolKey, IToolEntity> = {
   floor: {
     disabled: true,
     src: "./icons/floor_icon.svg",
-    key: ["2"],
+    keyBindings: ["2"],
     alt: "floor icon",
     width: 33,
     height: 23.5,
@@ -36,7 +36,7 @@ const TOOLS: Record<IToolKey, IToolEntity> = {
   basement: {
     limit: 1,
     disabled: false,
-    key: ["1"],
+    keyBindings: ["1"],
     src: "./icons/basement_icon.svg",
     alt: "basement icon",
     width: 37,
@@ -44,66 +44,75 @@ const TOOLS: Record<IToolKey, IToolEntity> = {
   },
 };
 
-export default function () {
+export default function Toolbox() {
   return (
     <aside className="w-fit">
       <ol className="grid gap-2 p-2 place-items-center-safe border-2">
-        {Object.keys(TOOLS).map((toolKey) => {
-          const tool = TOOLS[toolKey as IToolKey];
+        {(Object.keys(TOOLS) as IToolKey[]).map((toolKey) => {
+          const tool = TOOLS[toolKey];
 
           return (
             <li key={toolKey} className="grid place-content-center-safe">
-              <button
-                type="button"
-                aria-label={toolKey}
-                className="relative p-2 w-11 h-11 border-2 cursor-pointer disabled:cursor-not-allowed hover:shadow-[inset_0px_0px_1px_0.5px_var(--color-blue-800)] active:shadow-[inset_0px_0px_0px_1px_var(--color-blue-800)]"
-                disabled={tool.disabled}
-              >
-                {tool.disabled && (
-                  <div
-                    aria-hidden={true}
-                    className="absolute pointer-events-none w-[105%] h-[105%] translate-x-[-20%] translate-y-[-27%] bg-black/70 "
-                  ></div>
-                )}
-
-                <Image
-                  aria-hidden="true"
-                  src={tool.src}
-                  alt={tool.alt}
-                  width={tool.width}
-                  height={tool.height}
-                  id={`${toolKey}_icon`}
-                  className="pointer-events-none"
-                  priority
-                />
-
-                {tool.limit && (
-                  <span
-                    className={`absolute pointer-events-none top-0 left-0 translate-x-[-57%] translate-y-[-50%] scale-60 text-sm font-semibold text-blue-800 bg-white rounded-full w-6 border-2 ${tool.disabled && "bg-black/50"}`}
-                  >
-                    {tool.limit}
-                  </span>
-                )}
-
-                <span
-                  className={`absolute pointer-events-none bottom-0 right-0 translate-x-[-15%] translate-y-[-30%] text-sm/2 bg-background/90 ${tool.disabled && "bg-inherit"}`}
-                >
-                  {tool.key[0]}
-                </span>
-
-                {tool.disabled && tool.limit && (
-                  <span
-                    aria-hidden={true}
-                    className="absolute pointer-events-none top-0 left-0 translate-x-[-57%] translate-y-[-50%] scale-60 text-sm font-semibold rounded-full w-6 border-2 bg-black/70"
-                  >
-                    <span className="opacity-0">{tool.limit}</span>
-                  </span>
-                )}
-              </button>
+              <ToolBtn tool={tool} toolKey={toolKey} />
             </li>
           );
         })}
       </ol>
     </aside>
+  );
+}
+
+// room for the useContext
+function ToolBtn({ tool, toolKey }: { tool: IToolEntity; toolKey: IToolKey }) {
+  return (
+    <button
+      type="button"
+      aria-label={toolKey}
+      className="relative p-2 w-11 h-11 border-2 cursor-pointer disabled:cursor-not-allowed hover:shadow-[inset_0px_0px_1px_0.5px_var(--color-blue-800)] active:shadow-[inset_0px_0px_0px_1px_var(--color-blue-800)] disabled:shadow-[inset_0px_0px_0px_20px_var(--color-neutral-600)]"
+      disabled={tool.disabled}
+    >
+      <Image
+        aria-hidden="true"
+        src={tool.src}
+        alt={tool.alt}
+        width={tool.width}
+        height={tool.height}
+        id={`${toolKey}_icon`}
+        className={`pointer-events-none ${tool.disabled && "grayscale opacity-50"}`}
+        priority
+      />
+
+      <LimitBadge tool={tool} />
+      <KeyBadge tool={tool} />
+    </button>
+  );
+}
+
+function KeyBadge({ tool }: { tool: IToolEntity }) {
+  if (!tool) throw Error("KeyBadge: tool prop was not specified");
+
+  return (
+    <span
+      aria-hidden
+      className={`absolute pointer-events-none bottom-0 right-0 translate-x-[-15%] translate-y-[-30%] text-sm/2 bg-background/90 ${tool.disabled && "bg-inherit"}`}
+    >
+      {tool.keyBindings[0]}
+    </span>
+  );
+}
+
+function LimitBadge({ tool }: { tool: IToolEntity }) {
+  if (!tool) throw Error("LimitBadge: tool prop was not specified");
+
+  return (
+    <>
+      {tool.limit && (
+        <span
+          className={`absolute pointer-events-none top-0 left-0 translate-x-[-57%] translate-y-[-50%] scale-60 text-sm font-semibold rounded-full w-6 border-2 ${tool.disabled ? "bg-neutral-600" : "text-blue-800 bg-white "}`}
+        >
+          {tool.limit}
+        </span>
+      )}
+    </>
   );
 }
