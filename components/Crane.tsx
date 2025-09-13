ff95c62c00cd9e7c88c4b169a0e39213e656bb4f
EGRrqq <egrrqqdev@gmail.com>
2025-09-13 14:36:38 +0500

    feat: crane cleanup, remove old basement img

--- components/Crane.tsx
+++ components/Crane.tsx
@@ -15,7 +15,7 @@ interface ICrane {
 const CRANE_ID = "crane";
 
 /**
- * Crane component — positions an image at the top and moves it horizontally
+ * crane component - positions an image at the top and moves it horizontally
  * based on pointer X. Accepts optional props for maxOffset and width/height.
  */
 export default function Crane({
@@ -23,7 +23,7 @@ export default function Crane({
   alt,
   width = 175,
   height = 72,
-  maxOffset = 250,
+  maxOffset = 200,
 }: ICrane) {
   if (!src) throw Error("src prop for the image was not specified");
   if (!alt) throw Error("alt prop for the image was not specified");
@@ -32,29 +32,17 @@ export default function Crane({
   usePointerParallax(elRef, { maxOffset });
 
   return (
-    <>
-      <section id={CRANE_ID} className="flex justify-center-safe">
-        <Image
-          src={src}
-          alt={alt}
-          width={width}
-          height={height}
-          ref={elRef}
-          style={{ willChange: "transform", touchAction: "none" }}
-          priority
-
-          // basically i need smth like grab prop that grabs tools from toolbox
-        />
-      </section>
-      <div className="flex justify-center-safe">
-        <Image
-          src="./basement.svg"
-          alt="basement"
-          width={465}
-          height={456}
-          priority
-        />
-      </div>
-    </>
+    <div id={CRANE_ID} className="flex justify-center-safe">
+      <Image
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        ref={elRef}
+        style={{ willChange: "transform", touchAction: "none" }}
+        // basically i need smth like grab prop that grabs tools from toolbox
+        priority
+      />
+    </div>
   );
 }
