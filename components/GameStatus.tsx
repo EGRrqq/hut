e3b4fe66ec4f33dd9b7535fb53d6127ddc83458e
EGRrqq <egrrqqdev@gmail.com>
2025-09-15 12:43:42 +0500

    feat: GameStatus, add selected tool status

--- components/GameStatus.tsx
+++ components/GameStatus.tsx
@@ -5,7 +5,7 @@ export function GameStatus() {
   const { gameData } = useGame();
 
   return (
-    <ul className="absolute grid right-0 top-50 [&>li]:flex [&>li]:justify-between [&>li]:gap-2 border-1 p-1">
+    <ul className="absolute grid gap-1 right-0 top-50 [&>li]:flex [&>li]:justify-between [&>li]:gap-2 border-1 p-1 w-50">
       <li>
         <p>game status: </p>
         <p>{gameData.state.isRoofPlaced ? "ended" : "playing"}</p>
@@ -22,6 +22,15 @@ export function GameStatus() {
       <li>
         <p>roof: </p>
         <p>{gameData.state.isRoofPlaced ? 1 : 0}</p>
+      </li>
+      <hr />
+      <li>
+        <p>selected tool: </p>
+        <p>
+          {gameData.state.selectedTool
+            ? gameData.state.selectedTool
+            : "no tool"}
+        </p>
       </li>
     </ul>
   );
