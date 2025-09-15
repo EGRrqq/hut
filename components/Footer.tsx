44cb3cd707db40acd4e933f6b57a35a2ffd2b26d
EGRrqq <egrrqqdev@gmail.com>
2025-09-15 12:30:04 +0500

    feat(Footer): reduce padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export interface IFooterProps {
 
 export function Footer({ children }: IFooterProps) {
   return (
-    <footer className="sticky bottom-0 left-full grid gap-2 [&>*:not(hr)]:hover:text-blue-800 w-fit p-5 border-1">
+    <footer className="sticky bottom-0 left-full grid gap-2 [&>*:not(hr)]:hover:text-blue-800 w-fit p-1 border-1">
       {children}
       <hr className="w-full" />
       <a href="http://t.me/egrqq" rel="noopener noreferrer" target="_blank">
