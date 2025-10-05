e71b06d9daf191c0bd26ffe5f51b80927d25b31f
EGRrqq <egrrqqdev@gmail.com>
2025-10-05 12:56:41 +0500

    feat: remove prisma

---
+++ express/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+const PORT = 5000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/api/v1/players', (req, res) => {
+    const players = [{ name: "serega", score: 52 }, { name: "vitaly", score: 15 }]
+    res.status(200).json(players)
+})
+
+app.listen(PORT, () => { console.log(`app runnin on port: ${PORT}`) })
--- server/prisma/migrations/20251003110243_player/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Player" (
-    "id" SERIAL NOT NULL,
-    "nickname" TEXT NOT NULL,
-    "score" INTEGER NOT NULL,
-
-    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
-);
