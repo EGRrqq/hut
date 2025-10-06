f6a82b3fddefd93eff3c0cafc9b6ba0e90db609e
EGRrqq <egrrqqdev@gmail.com>
2025-10-06 13:26:45 +0500

    feat: move api_url into env variable for dev and prod separatly

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    const isProduction = process.env.NODE_ENV === 'production';
-
     return [{
       source: "/api/:path*",
-      destination: isProduction ? "https://hut-server.vercel.app/api/:path*" : "http://localhost:5000/api/:path*"
+      destination: `${process.env.API_URL}/:path*`
     }];
   },
 };
